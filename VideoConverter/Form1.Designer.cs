95d6f2e97b05571f9dc88aa7a602bfa995787c29
jmaus <jmaus@MAUSMACHINE>
2025-12-09 23:33:58 -0500

    Add project files.

---
+++ VideoConverter/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace VideoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            comboBoxFrameRate = new ComboBox();
+            comboBoxBitrate = new ComboBox();
+            comboBoxCodec = new ComboBox();
+            comboBoxInterpolation = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button2 = new Button();
+            label7 = new Label();
+            button3 = new Button();
+            progressBar1 = new ProgressBar();
+            labelProgress = new Label();
+            txtFileName = new TextBox();
+            label6 = new Label();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(24, 24);
+            button1.Name = "button1";
+            button1.Size = new Size(256, 53);
+            button1.TabIndex = 0;
+            button1.Text = "Select Video";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(24, 80);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 25);
+            label1.TabIndex = 1;
+            // 
+            // comboBoxFrameRate
+            // 
+            comboBoxFrameRate.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxFrameRate.FormattingEnabled = true;
+            comboBoxFrameRate.Items.AddRange(new object[] { "23.976", "24", "25", "29.97", "50", "59.94" });
+            comboBoxFrameRate.Location = new Point(195, 115);
+            comboBoxFrameRate.Name = "comboBoxFrameRate";
+            comboBoxFrameRate.Size = new Size(223, 33);
+            comboBoxFrameRate.TabIndex = 2;
+            // 
+            // comboBoxBitrate
+            // 
+            comboBoxBitrate.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxBitrate.FormattingEnabled = true;
+            comboBoxBitrate.Items.AddRange(new object[] { "15M", "18M", "20M", "21M", "22M", "23M", "24M", "25M", "26M", "27M", "28M", "29M", "30M", "35M" });
+            comboBoxBitrate.Location = new Point(195, 165);
+            comboBoxBitrate.Name = "comboBoxBitrate";
+            comboBoxBitrate.Size = new Size(223, 33);
+            comboBoxBitrate.TabIndex = 3;
+            // 
+            // comboBoxCodec
+            // 
+            comboBoxCodec.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxCodec.Items.AddRange(new object[] { "libx264", "mpeg2video", "vc1" });
+            comboBoxCodec.Location = new Point(195, 215);
+            comboBoxCodec.Name = "comboBoxCodec";
+            comboBoxCodec.Size = new Size(223, 33);
+            comboBoxCodec.TabIndex = 4;
+            // 
+            // comboBoxInterpolation
+            // 
+            comboBoxInterpolation.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxInterpolation.FormattingEnabled = true;
+            comboBoxInterpolation.Items.AddRange(new object[] { "minterpolate", "tblend", "None" });
+            comboBoxInterpolation.Location = new Point(195, 265);
+            comboBoxInterpolation.Name = "comboBoxInterpolation";
+            comboBoxInterpolation.Size = new Size(223, 33);
+            comboBoxInterpolation.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(162, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Frames per second";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 25);
+            label3.TabIndex = 7;
+            label3.Text = "Bitrate";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 215);
+            label4.Name = "label4";
+            label4.Size = new Size(62, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Codec";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(27, 265);
+            label5.Name = "label5";
+            label5.Size = new Size(114, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Interpolation";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(403, 397);
+            button2.Name = "button2";
+            button2.Size = new Size(178, 80);
+            button2.TabIndex = 10;
+            button2.Text = "Convert";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(24, 458);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 25);
+            label7.TabIndex = 13;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(24, 397);
+            button3.Name = "button3";
+            button3.Size = new Size(253, 58);
+            button3.TabIndex = 12;
+            button3.Text = "Select Output Directory";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(24, 531);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(557, 23);
+            progressBar1.TabIndex = 14;
+            // 
+            // labelProgress
+            // 
+            labelProgress.Location = new Point(588, 531);
+            labelProgress.Name = "labelProgress";
+            labelProgress.Size = new Size(50, 23);
+            labelProgress.TabIndex = 15;
+            labelProgress.Text = "0%";
+            // 
+            // txtFileName
+            // 
+            txtFileName.Location = new Point(27, 327);
+            txtFileName.Name = "txtFileName";
+            txtFileName.Size = new Size(391, 31);
+            txtFileName.TabIndex = 16;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(424, 333);
+            label6.Name = "label6";
+            label6.Size = new Size(130, 25);
+            label6.TabIndex = 17;
+            label6.Text = "New File Name";
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(339, 24);
+            button4.Name = "button4";
+            button4.Size = new Size(256, 53);
+            button4.TabIndex = 18;
+            button4.Text = "Concatenate Videos";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(656, 591);
+            Controls.Add(button4);
+            Controls.Add(label6);
+            Controls.Add(txtFileName);
+            Controls.Add(label7);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBoxInterpolation);
+            Controls.Add(comboBoxCodec);
+            Controls.Add(comboBoxBitrate);
+            Controls.Add(comboBoxFrameRate);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(progressBar1);
+            Controls.Add(labelProgress);
+            Name = "Form1";
+            Text = "Video Converter";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private ComboBox comboBoxFrameRate;
+        private ComboBox comboBoxBitrate;
+        private ComboBox comboBoxCodec;
+        private ComboBox comboBoxInterpolation;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button2;
+        private Label label7;
+        private Button button3;
+        private ProgressBar progressBar1;
+        private Label labelProgress;
+        private TextBox txtFileName;
+        private Label label6;
+        private Button button4;
+    }
+}
---
+++ VideoConverter/Form1.cs
@@ -0,0 +1,257 @@
+namespace VideoConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Video Files|*.mp4;*.avi;*.mov;*.wmv;*.mkv|All Files|*.*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    label1.Text = openFileDialog.FileName;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxFrameRate.SelectedItem = "29.97";
+            comboBoxBitrate.SelectedItem = "25M";
+            comboBoxCodec.SelectedItem = "libx264";
+            comboBoxInterpolation.SelectedItem = "minterpolate";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    label7.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            // Get input file and output directory
+            string inputFile = label1.Text;
+            string outputDir = label7.Text;
+            string newFileName = txtFileName.Text.Trim() ?? "outputVideo.mp4";
+            if (!newFileName.EndsWith(".mp4", StringComparison.OrdinalIgnoreCase))
+            {
+                newFileName += ".mp4";
+            }
+            if (string.IsNullOrWhiteSpace(inputFile) && string.IsNullOrWhiteSpace(outputDir))
+            {
+                MessageBox.Show("Please select both an input file and output directory.");
+                return;
+            }
+
+            // Validation: require input file and output directory
+            if (string.IsNullOrWhiteSpace(inputFile))
+            {
+                MessageBox.Show("You must select an input video file before converting.", "Missing Input File", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(outputDir))
+            {
+                MessageBox.Show("You must select an output folder before converting.", "Missing Output Folder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Get options from ComboBoxes
+            string frameRate = comboBoxFrameRate.SelectedItem?.ToString() ?? "29.97";
+            string bitrate = comboBoxBitrate.SelectedItem?.ToString()?.Replace(" Mbps", "M") ?? "25M";
+            string codec = comboBoxCodec.SelectedItem?.ToString() ?? "libx264";
+            string interpolation = comboBoxInterpolation.SelectedItem?.ToString() ?? "minterpolate";
+
+            // Build output file path
+            string baseName = Path.GetFileNameWithoutExtension(newFileName);
+            string ext = Path.GetExtension(newFileName);
+            string outputFile = Path.Combine(outputDir, newFileName);
+            int count = 1;
+            while (File.Exists(outputFile))
+            {
+                outputFile = Path.Combine(outputDir, $"{baseName} ({count}){ext}");
+                count++;
+            }
+
+            // Build ffmpeg arguments with conditional -vf and -r
+            string vfArg = "";
+            string rArg = $"-r {frameRate} ";
+            if (interpolation.Equals("minterpolate", StringComparison.OrdinalIgnoreCase))
+            {
+                vfArg = $"-vf \"minterpolate=fps={frameRate}\" ";
+                rArg = ""; // Do not add -r if minterpolate is used
+            }
+            else if (interpolation.Equals("tblend", StringComparison.OrdinalIgnoreCase))
+            {
+                vfArg = "-vf \"tblend=all_mode=average\" ";
+            }
+            else if (interpolation.Equals("None", StringComparison.OrdinalIgnoreCase))
+            {
+                vfArg = "";
+            }
+
+            // Determine input argument based on file type
+            string inputArg;
+            if (inputFile.EndsWith(".txt", StringComparison.OrdinalIgnoreCase))
+            {
+                inputArg = $"-f concat -safe 0 -i \"{inputFile}\" ";
+            }
+            else
+            {
+                inputArg = $"-i \"{inputFile}\" ";
+            }
+
+            string args = $"{inputArg}{vfArg}{rArg}-b:v {bitrate} -c:v {codec} -profile:v high -level 4.1 \"{outputFile}\"";
+
+            progressBar1.Value = 0;
+            labelProgress.Text = "0%";
+
+            // Get duration first
+            TimeSpan? duration = await GetVideoDurationAsync(inputFile);
+            if (duration == null)
+            {
+                MessageBox.Show("Could not determine video duration.");
+                return;
+            }
+
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = "ffmpeg.exe";
+            process.StartInfo.Arguments = args;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.EnableRaisingEvents = true;
+
+            process.Start();
+
+            await Task.Run(() =>
+            {
+                string line;
+                var stderr = process.StandardError;
+                TimeSpan lastTime = TimeSpan.Zero;
+                while ((line = stderr.ReadLine()) != null)
+                {
+                    var time = ParseFfmpegTime(line);
+                    if (time != null && duration.Value.TotalSeconds > 0)
+                    {
+                        int percent = (int)(time.Value.TotalSeconds / duration.Value.TotalSeconds * 100);
+                        if (percent > 100) percent = 100;
+                        this.Invoke(new Action(() =>
+                        {
+                            progressBar1.Value = percent;
+                            labelProgress.Text = percent + "%";
+                        }));
+                    }
+                }
+                process.WaitForExit();
+            });
+
+            // Check if file was created
+            if (System.IO.File.Exists(outputFile))
+            {
+                progressBar1.Value = 100;
+                labelProgress.Text = "100%";
+                MessageBox.Show("Conversion completed successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Conversion failed. Output file was not created.");
+            }
+        }
+
+        private async Task<TimeSpan?> GetVideoDurationAsync(string inputFile)
+        {
+            return await Task.Run<TimeSpan?>(() =>
+            {
+                try
+                {
+                    var probe = new System.Diagnostics.Process();
+                    probe.StartInfo.FileName = "ffmpeg.exe";
+                    probe.StartInfo.Arguments = $"-i \"{inputFile}\"";
+                    probe.StartInfo.UseShellExecute = false;
+                    probe.StartInfo.RedirectStandardError = true;
+                    probe.StartInfo.RedirectStandardOutput = true;
+                    probe.StartInfo.CreateNoWindow = true;
+                    probe.Start();
+                    string output = probe.StandardError.ReadToEnd();
+                    probe.WaitForExit();
+                    var match = System.Text.RegularExpressions.Regex.Match(output, @"Duration: (\d+):(\d+):(\d+).(\d+)");
+                    if (match.Success)
+                    {
+                        int h = int.Parse(match.Groups[1].Value);
+                        int m = int.Parse(match.Groups[2].Value);
+                        int s = int.Parse(match.Groups[3].Value);
+                        int ms = int.Parse(match.Groups[4].Value) * 10;
+                        return new TimeSpan(0, h, m, s, ms);
+                    }
+                }
+                catch { }
+                return null;
+            });
+        }
+
+        private TimeSpan? ParseFfmpegTime(string line)
+        {
+            // ffmpeg outputs: ... time=00:01:23.45 ...
+            var match = System.Text.RegularExpressions.Regex.Match(line, @"time=(\d+):(\d+):(\d+)\.(\d+)");
+            if (match.Success)
+            {
+                int h = int.Parse(match.Groups[1].Value);
+                int m = int.Parse(match.Groups[2].Value);
+                int s = int.Parse(match.Groups[3].Value);
+                int ms = int.Parse(match.Groups[4].Value) * 10;
+                return new TimeSpan(0, h, m, s, ms);
+            }
+            return null;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Select multiple video files
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Video Files|*.mp4;*.avi;*.mov;*.wmv;*.mkv|All Files|*.*";
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string outputDir = label7.Text;
+                    if (string.IsNullOrWhiteSpace(outputDir))
+                    {
+                        MessageBox.Show("You must select an output folder first.", "Missing Output Folder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    // Find next available vidClip_{number}.txt
+                    int fileNum = 1;
+                    string txtFile;
+                    do
+                    {
+                        txtFile = Path.Combine(outputDir, $"vidClip_{fileNum}.txt");
+                        fileNum++;
+                    } while (File.Exists(txtFile));
+
+                    // Write selected files to txtFile
+                    using (var writer = new StreamWriter(txtFile))
+                    {
+                        foreach (var file in openFileDialog.FileNames)
+                        {
+                            string fileName = Path.GetFileName(file);
+                            writer.WriteLine($"file '{fileName}'");
+                        }
+                    }
+                    MessageBox.Show($"Text file created: {Path.GetFileName(txtFile)}. Use this txt file as video input.");                    
+                }
+            }
+        }
+    }
+}
